334b342fec04c9e7b84aac2526a9ebe985c75229
Borjues <dhani.535230149@stu.untar.ac.id>
2025-05-21 22:41:55 +0700

    Balikkan ke manual (sementara)

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -3,11 +3,12 @@ from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 
 stemmer = StemmerFactory().create_stemmer()
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 stopwords = set(StopWordRemoverFactory().get_stop_words())
 
 def preprocess(text):
     text = text.lower()
     text = re.sub(r'[^\w\s]', '', text)
     words = text.split()
-    words = [word for word in words if word not in stopwords]
-    return " ".join([stemmer.stem(word) for word in words])
+    words = [w for w in words if w not in stopwords]
+    return " ".join([stemmer.stem(w) for w in words])
